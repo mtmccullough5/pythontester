d872470caffed46851fbe98659eb8da900f105be
Matthew McCullough <mcculloughmatthew5@gmail.com>
2017-07-30 20:34:01 +0000

    Python Commit 0.01

---
+++ BasicTesters1.py
@@ -0,0 +1,1 @@
+% This is a basic Python test to test the use of cloud9 IDE.
---
+++ BasicTesters2.py
@@ -0,0 +1,8 @@
+# This is a second file to try out 
+OneFish = 'One Fish'
+TwoFish = 'two fish'
+RedFish = 'red fish'
+BlueFish = 'blue fish'
+
+DrS = OneFish+TwoFish+RedFish+BlueFish
+print(DrS)
